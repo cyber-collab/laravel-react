8acb74f9e87a8cf8da7d9d96de3062bb3fee1d21
Den <denyszkramar@gmail.com>
2024-04-26 16:49:41 +0300

    feat: add employee

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employee;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'number' => $this->faker->randomNumber(9, true),
+            'company_id' => rand(1, 30),
+        ];
+    }
+}
